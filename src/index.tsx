8ef96950deccaaada6afa3a39ed6d7a8a8331da5
jianjundu <v@jianwi.cn>
2023-09-21 14:35:07 +0800

    优化样式

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,23 @@
-import React, {useEffect, useState} from 'react'
+import React, {useEffect, useState, useRef} from 'react'
 import ReactDOM from 'react-dom/client'
 import {bitable, HostContainerSize} from '@lark-base-open/js-sdk';
 import {Alert, AlertProps, Button, Select, Form} from 'antd';
 import axios from "axios";
-import { initI18n } from './i18n'
+import {initI18n} from './i18n'
 import {useTranslation} from "react-i18next";
+import * as monaco from 'monaco-editor';
 
+import editorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker'
+import tsWorker from 'monaco-editor/esm/vs/language/typescript/ts.worker?worker'
+
+self.MonacoEnvironment = {
+    getWorker(_, label) {
+        if (label === 'typescript' || label === 'javascript') {
+            return new tsWorker()
+        }
+        return new editorWorker()
+    }
+}
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
@@ -19,10 +31,10 @@ function App() {
     useEffect(() => {
         const timer = setTimeout(() => {
             initI18n('en');
-            setTimeout(()=>{
+            setTimeout(() => {
                 setLoadErr(<LoadErr/>)
 
-            },1000)
+            }, 1000)
         }, 5000)
         bitable.bridge.getLanguage().then((lang) => {
             clearTimeout(timer)
@@ -33,7 +45,16 @@ function App() {
     }, [])
 
     if (load) {
-        return <LoadApp />
+        return <LoadApp/>
+    }else {
+        return <div style={{
+            textAlign: "center",
+            marginTop: "100px"
+        }}>
+            <div style={{
+                marginTop: "10px"
+            }}>加载中...</div>
+        </div>
     }
 
     return loadErr
@@ -48,7 +69,6 @@ function LoadErr() {
 
 }
 
-
 function LoadApp() {
     const {t} = useTranslation();
     const [logs, setLogs] = useState("")
@@ -57,62 +77,47 @@ function LoadApp() {
     let templates = [
         {
             title: t("getActiveTable"),
-            code: `async function main(){
-    const table = await bitable.base.getActiveTable();
-    const tableName = await table.getName();
-    console.log(tableName)
-}
-        `
+            code: ["async function main(){", "    const table = await bitable.base.getActiveTable();", "    const tableName = await table.getName();", "    console.log(tableName)", "}"].join("\n")
         },
         {
             title: t("getActiveTableRecordList"),
-            code: ` async function main(){
-            const table = await bitable.base.getActiveTable();
-            const {recordIdList} = await table.getRecordList();
-            console.log(recordIdList)
-        }
-    `
+            code: ["async function main(){", "    const table = await bitable.base.getActiveTable();", "    const {recordIdList} = await table.getRecordList();", "    console.log(recordIdList)", "}"].join("\n")
         },
         {
             title: t("getActiveTableFieldList"),
-            code: ` async function main(){
-            const table = await bitable.base.getActiveTable();
-            const fields = await table.getFieldMetaList();
-            console.log(fields)
-            }`
+            code: ["async function main(){", "    const table = await bitable.base.getActiveTable();", "    const fields = await table.getFieldMetaList();", "    console.log(fields)", "}"].join("\n")
         },
         {
             title: t("httpExample"),
-            code: `async function main(){
-     let r = await axios.post("https://base-translator-api.replit.app/cell_translate",{
-                q: "测试脚本",
-                from: "zh",
-                to: "en"
-            },{
-                headers:{
-                    "Content-Type":"application/x-www-form-urlencoded"
-                }
-            })
-            console.log(r.data)
-}`
+            code: ["async function main(){", "    let r = await axios.post(\"https://base-translator-api.replit.app/cell_translate\",{", "                q: \"测试脚本\",", "                from: \"zh\",", "                to: \"en\"", "            },{", "                headers:{", "                    \"Content-Type\":\"application/x-www-form-urlencoded\"", "                }", "            })", "            console.log(r.data)", "}"].join("\n")
         },
         {
             title: t("addRecord"),
-            code: `async function main(){
-       let table = await bitable.base.getActiveTable();
-           let fields = await table.getFieldMetaList();
-           let fieldId = fields[0].id;
-           await table.addRecord({
-               fields: {
-                   [fieldId]: "${t('addNewLine')}"
-               }
-           })
-           console.log("${t('success')}")
-}`
+            code: ["async function main(){", "   let table = await bitable.base.getActiveTable();", "    let fields = await table.getFieldMetaList();", "    let fieldId = fields[0].id;", "    await table.addRecord({", "        fields: {", `            [fieldId]: \"${t('addNewLine')}\"`, "        }", "    })", `    console.log(\"${t('success')}\")`, "}"].join("\n")
         }
     ]
 
-    const [code, setCode] = useState(templates[0].code)
+    const container = useRef(null);
+    const editorRef = useRef(null);
+
+    useEffect(() => {
+        if (!container.current) return;
+        if (editorRef.current) {
+            return;
+        }
+        let editor = monaco.editor.create(container.current, {
+            value: templates[0].code,
+            language: "javascript",
+            minimap: {
+                enabled: false
+            },
+            formatOnPaste: true,
+            formatOnType: true,
+            folding: true,
+        });
+        editorRef.current = editor
+    }, [])
+
 
     let log = function (...args: any) {
         setLogs((prevState) => {
@@ -124,46 +129,39 @@ function LoadApp() {
 
 
     return <div>
-        <h5 style={{
-            textAlign: "center"
-        }}>js code runner</h5>
-
         <div>
             <Form.Item label={t("example")}>
                 <Select style={{width: "100%"}}
                         onChange={(value) => {
-                            setCode(value)
+                            editorRef.current.setValue(value)
                         }}
                         defaultValue={templates[0].code}
                 >
                     {templates.map(item => {
-                        return <Select.Option value={item.code}>{item.title}</Select.Option>
+                        return <Select.Option key={item.title} value={item.code}>{item.title}</Select.Option>
                     })
                     }
                 </Select>
             </Form.Item>
         </div>
 
-        <div style={
-            {
-                display: "flex",
-                alignItems: "center"
-            }
-        }>
-      <textarea style={{
-          padding: "10px"
-      }}
-                value={code}
-                rows={10} cols={50} onChange={(res) => {
-          console.info(res.target.value)
-          setCode(res.target.value)
-      }}>{code}</textarea>
-            <div style={{marginLeft: "10px"}}></div>
-            <Button onClick={() => {
-                console.info("运行", code)
-                setLogs("")
+        <div style={{
+            width: "100%",
+            height: "190px",
+            overflow: "hidden",
+        }} ref={container}></div>
 
-                let codeText = `
+        <Button
+            type="primary"
+            style={{
+                marginTop: "10px"
+            }}
+            onClick={() => {
+            let code = editorRef.current.getValue()
+            console.info("运行", code)
+            setLogs("")
+
+            let codeText = `
                 console.log = log;
                 
                 ${code}
@@ -173,14 +171,13 @@ function LoadApp() {
                 }
                 `
 
-                try {
-                    let fn = new Function("bitable", "log", "axios", codeText)
-                    fn(bitable, log, axios)
-                } catch (e:any) {
-                    setErrorLogs(e.message)
-                }
-            }}>{t('run')}</Button>
-        </div>
+            try {
+                let fn = new Function("bitable", "log", "axios", codeText)
+                fn(bitable, log, axios)
+            } catch (e: any) {
+                setErrorLogs(e.message)
+            }
+        }}>{t('run')}</Button>
 
         <div style={{
             marginTop: "10px"
@@ -189,8 +186,9 @@ function LoadApp() {
             logs && <div style={{
                 backgroundColor: "#f5f5f5",
                 padding: "5px 10px",
-                width:"fit-content"
-            }}><pre>{logs}</pre>
+                width: "fit-content"
+            }}>
+                <pre>{logs}</pre>
             </div>
         }
 
